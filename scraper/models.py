36c6e0495ce05322e37950c5b8998a1d8adbd805
Renan Santana <phy.renan@gmail.com>
2015-01-13 00:13:00 -0500

    new

--- scraper/models.py
+++ scraper/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Movie(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=20)
 
     def __unicode__(self):
         return u'%s' % self.title
--- scraper/urls.py
+++ scraper/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from scraper.views import *
 
 urlpatterns = patterns('',
-
+    url(r'^$', welcome),
+    url(r'^search/$', search),
 )
